6dabf696a46492f916243759be508d5cf08fff1b
enzoaicardi <enzo.aicardi@gmail.com>
2021-04-10 16:30:32 +0200

    parallax commit

--- js/parallax.js
+++ js/parallax.js
@@ -15,7 +15,7 @@ function anime(){
     var v = st > jt ? st < jt+dh ? st-jt : dh : 0;
     var deg = (v/dh)*20;
 
-    body.style.backgroundPosition = 'left 0px top ' + st/2 + 'px';
+    // body.style.backgroundPosition = 'left 0px top ' + st/2 + 'px';
     navette.style.transform = 'translateY(' + st/1.5 + 'px)';
     astronaute.style.transform = 'translateY(' + deg*10 + 'px) rotateZ(-' + deg + 'deg)'; 
 
