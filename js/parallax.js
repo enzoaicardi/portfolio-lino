0da6e06e95b36e7d23b9789c59b5ac9c30b18f98
enzoaicardi <enzo.aicardi@gmail.com>
2021-04-10 17:52:23 +0200

    parallax commit

--- js/parallax.js
+++ js/parallax.js
@@ -5,7 +5,7 @@ var astronaute = document.querySelector('.astronaute');
 var job = document.querySelector('.job');
 
 // document.addEventListener('scroll', Parallax, {passive:true});
-var clock = setInterval(Parallax, 20);
+// var clock = setInterval(Parallax, 20);
 
 function anime(){
 
@@ -16,7 +16,7 @@ function anime(){
     var v = st > jt ? st < jt+dh ? st-jt : dh : 0;
     var deg = (v/dh)*20;
 
-    // body.style.backgroundPosition = 'left 0px top ' + st/2 + 'px';
+    body.style.backgroundPosition = 'left 0px top ' + st/2 + 'px';
     navette.style.transform = 'translateY(' + st/1.5 + 'px)';
     astronaute.style.transform = 'translateY(' + deg*10 + 'px) rotateZ(-' + deg + 'deg)'; 
 
