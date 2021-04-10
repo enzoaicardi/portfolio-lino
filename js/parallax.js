b285962f2e4f1dd4ae3a75c805588c990870d8db
enzoaicardi <enzo.aicardi@gmail.com>
2021-04-10 17:25:29 +0200

    parallax commit

--- js/parallax.js
+++ js/parallax.js
@@ -3,13 +3,12 @@ var body = document.body;
 var navette = document.querySelector('.navette');
 var astronaute = document.querySelector('.astronaute');
 var job = document.querySelector('.job');
-var pid = 0;
 
-document.addEventListener('scroll', Parallax, {passive:true});
+// document.addEventListener('scroll', Parallax, {passive:true});
+var clock = setInterval(Parallax, 20);
 
 function anime(){
 
-    pid++;
     var st = body.scrollTop;
     var jt = job.offsetTop;
     var dh = window.innerHeight;
@@ -17,16 +16,12 @@ function anime(){
     var v = st > jt ? st < jt+dh ? st-jt : dh : 0;
     var deg = (v/dh)*20;
 
-    if(pid >= 3){
+    body.style.backgroundPosition = 'left 0px top ' + st/2 + 'px';
+    navette.style.transform = 'translateY(' + st/1.5 + 'px)';
+    astronaute.style.transform = 'translateY(' + deg*10 + 'px) rotateZ(-' + deg + 'deg)'; 
 
-        pid = 0;
-        // body.style.backgroundPosition = 'left 0px top ' + st/2 + 'px';
-        navette.style.transform = 'translateY(' + st/1.5 + 'px)';
-        astronaute.style.transform = 'translateY(' + deg*10 + 'px) rotateZ(-' + deg + 'deg)'; 
-
-    }
 }
 
-function Parallax(e){
+function Parallax(){
     requestAnimationFrame(anime);
 }
